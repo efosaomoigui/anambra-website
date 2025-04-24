85d9a171b62563608631f4e13749259062033347
Omoigui Efosa Abel <efe.omoigui@gmail.com>
2025-04-24 08:47:06 -0400

    UI background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,27 +3,30 @@ import SearchComponent from "./SearchComponent";
 export default function Hero() {
   return (
     <section className="text-center px-4 relative overflow-hidden font-instrument">
-      {/* Welcome Text Container with Background Shape */}
+      {/* Welcome Text Container */}
       <div className="relative pt-[100px] sm:pt-[140px] md:pt-[180px] inline-block">
-        {/* Rotated Yellow Background Shape behind text only */}
-        <div
-          className="absolute -bottom-4 left-1/2 -translate-x-1/2 z-0"
-          style={{
-            width: "310.35px",
-            height: "90.45px",
-            backgroundColor: "#FFD700",
-            transform: "rotate(-4deg)",
-            borderRadius: "10px",
-          }}
-        ></div>
-
-        {/* Welcome Text */}
         <h1
           className="relative z-10 text-3xl sm:text-4xl md:text-5xl font-bold text-black leading-snug"
           style={{ fontFamily: "Integral CF" }}
         >
           <p>GOVERNMENT OF</p>
-          <p>ANAMBRA STATE</p>
+          <p>
+            <span className="relative inline-block">
+              {/* Yellow Background Behind "ANAMBRA" Only */}
+              <span
+                className="absolute inset-0 -rotate-[0deg] -z-10"
+                style={{
+                  backgroundColor: "#FFD700",
+                  borderRadius: "10px",
+                  transformOrigin: "center",
+                  marginTop: "3px",
+                  marginBottom: "2px",
+                }}
+              ></span>
+              <span className="relative px-2">ANAMBRA</span>
+            </span>{" "}
+            STATE
+          </p>
         </h1>
       </div>
 
